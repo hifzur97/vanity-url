ab94ab95a90376c8f007c691bdbcb3e7923c1ffd
hifzur-rahman <hifzur.ezzoom@gmail.com>
2021-10-21 19:34:28 +0000

    vanity url api service

---
+++ routes/redirect.js
@@ -0,0 +1,32 @@
+const express = require('express')
+
+const router = express.Router()
+
+const Url = require('../models/Url')
+
+// : app.get(/:code)==>end of a string
+// 
+
+// @route       GET /:code
+// @description    Redirect to the long/original URL 
+router.get('/:code', async(req, res)=>{
+    console.log(" ----- req.params.code ----",req.params.code)
+    try{
+        const url = await Url.findOne({urlCode: req.params.code})
+        console.log( '-------- url ----',url)
+        if(url){
+            return res.redirect(url.longUrl)
+        }
+        else{
+            return res.status(404).json('No URL Found')
+        }
+
+    }
+    catch(err){
+        console.error(err)
+        res.status(500).json('Server Error')
+    }
+})
+
+
+module.exports = router
